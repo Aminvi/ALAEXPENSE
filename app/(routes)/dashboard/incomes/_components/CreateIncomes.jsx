124c287420f65df6a4a4957b2484e213fddb73a9
Adesokan Al-AMEEN <112561640+Aminvi@users.noreply.github.com>
2024-08-20 22:30:28 +0100

    Update CreateIncomes.jsx

--- app/(routes)/dashboard/incomes/_components/CreateIncomes.jsx
+++ app/(routes)/dashboard/incomes/_components/CreateIncomes.jsx
@@ -1,5 +1,8 @@
 "use client";
 import React, { useState } from "react";
+import Button from '@/components/ui/button';
+import AlertDialog from '@/components/ui/alert-dialog';
+
 import {
   Dialog,
   DialogClose,
